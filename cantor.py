298ee53792ffeaa18cc8f2c29b905d4150999785
Ross Warkentin <ross.warkentin@gmail.com>
2023-08-02 21:33:40 -0500

    adding some docs and clues for optimization later

--- cantor.py
+++ cantor.py
@@ -7,6 +7,12 @@ def diagonal(r, c):
     return c
 
 def compute_cantor_index(row, col):
+    """ break up the cantor matrix computation into two steps:
+        1. compute the values along each diagonal using some simple
+            triangle geometry
+        2. increment each of the values along the diagonal based on their column
+            index
+    """
     return triangle(row, col) + diagonal(row, col)
 
 
@@ -14,8 +20,9 @@ def get_code_val(row, col):
     cantor_index = compute_cantor_index(row-1, col-1)
 
     value = 20151125
-    for i in range(int(cantor_index)):
-        value = value * 252533 % 33554393
+    for _ in range(int(cantor_index)):
+        # todo: I suspect there is some optimizations that can be done here to quickly compute the final value without needing to iterate from 1 to cantor_index
+        value = value * 252533 % 33554393 
 
     print(value)
 
